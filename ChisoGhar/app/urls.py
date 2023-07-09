8239e59a201e90c57e87529730c8fd25de6b2f08
Suman Khadka <dnatsu022@gmail.com>
2023-07-09 17:40:56 +0545

    added user authentication

--- ChisoGhar/app/urls.py
+++ ChisoGhar/app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth import views as auth_view
+from . forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
 
 urlpatterns = [
     path("", views.home),
@@ -10,4 +12,26 @@ urlpatterns = [
     path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
     path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
     path("product-detail/<int:pk>", views.ProductDetail.as_view(), name="product-detail"),
+    path("profile/", views.ProfileView.as_view(), name='profile'),
+    path("address/", views.address, name='address'),
+    path("updateAddress/<int:pk>", views.updateAddress.as_view(), name="updateAddress"),
+
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
+    path('checkout/', views.checkout.as_view(), name='checkout'),
+
+    path('pluscart/', views.plus_cart),
+    path('minuscart/', views.minus_cart),
+    path('removecart/', views.remove_cart),
+
+    path('esewa-request/', views.EsewaRequestView.as_view(), name='esewarequest'),
+
+    # login authentication
+    path('registration/', views.CustomerRegistrationView.as_view(), name= 'customerregistration'),
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
+    path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'),
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
